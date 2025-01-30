d235c7e8eaf470fbb9a9dfa92032de7e47f389d1
Jeremie Levy <jeremylevy.131290@gmail.com>
2025-01-30 16:36:09 +0100

    Match.java class model ok

--- src/main/java/com/scorestable/restapi/Match.java
+++ src/main/java/com/scorestable/restapi/Match.java
@@ -11,5 +11,74 @@ import java.util.Objects;
 @Table(name = "matches")
 
 public class Match {
-    
+    @Id
+    @GeneratedValue(strategy = GeneratioType.IDENTITY)
+    private Long id;
+
+    private String sport;
+    private String team1;
+    private String team2;
+    private Integer score1;
+    private Integer score2;
+
+    public Match() {
+    }
+
+    public Match(String sport, String team1, String team2, Integer score1, Integer score2) {
+        this.sport = sport;
+        this.team1 = team1;
+        this.team2 = team2;
+        this.score1 = score1;
+        this.score2 = score2;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public String getTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(String team1) {
+        this.team1 = team1;
+    }
+
+    public String getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(String team2) {
+        this.team2 = team2;
+    }
+
+    public Integer getScore1() {
+        return score1;
+    }
+
+    public void setScore1(Integer score1) {
+        this.score1 = score1;
+    }
+
+    public Integer getScore2() {
+        return score2;
+    }
+
+    public void setScore2(Integer score2) {
+        this.score2 = score2;
+    }
+
+
 }
