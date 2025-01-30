dc3513fcef0844135fc2666f8c661ebc7772be20
Jeremie Levy <jeremylevy.131290@gmail.com>
2025-01-30 16:52:39 +0100

    MatchRepository ok

---
+++ src/main/java/com/scorestable/restapi/MatchRepository.java
@@ -0,0 +1,8 @@
+package com.scorestable.restapi;
+import com.scorestable.restapi.model.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match, Long> {
+}
