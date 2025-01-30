483928c816ab6c25183f38322c00fd2f0183659a
Jeremie Levy <jeremylevy.131290@gmail.com>
2025-01-30 22:45:32 +0100

    MatchController ok

---
+++ src/main/java/com/scorestable/restapi/MatchController.java
@@ -0,0 +1,40 @@
+package com.scorestable.restapi;
+
+import com.scorestable.restapi.model.Match;
+import com.scorestable.restapi.service.MatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/matches")
+public class MatchController {
+
+    @Autowired
+    private MatchService matchService;
+
+    @GetMapping
+    public List<Match> getAllMatches() {
+        return matchService.getAllMatches();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Match> getMatchById(@PathVariable Long id) {
+        Optional<Match> match = matchService.getMatchById(id);
+        return match.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Match createMatch(@RequestedBody Match match) {
+        return matchService.createMatch(match) ;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMatch(@PathVariable Long id) {
+        matchService.deleteMatch(id);
+        return ResponseEntity.noContent().build();
+    }
+}
